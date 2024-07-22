74c9eafa65335657299f81d7e4ea00416e20a8d2
c0b22065c3 <c0b22065c3@edu.teu.ac.jp>
2024-07-22 19:19:17 +0900

    ギターからの入力をできるようにした

---
+++ Guitar/Assets/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private string sceneName; // 現在のシーンの名前を格納する用
+    private bool startGame = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 現在のシーンの名前を格納
+        sceneName = SceneManager.GetActiveScene().name;
+
+        //シーンが切り替わった時に呼ばれるメソッドを登録
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !startGame)
+        {
+            SceneManager.LoadScene("Battle");
+            startGame = true;
+        }
+    }
+
+    void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+    {
+        sceneName = nextScene.name;
+    }
+}
